dfa696a8f99354ccf14db4948e8000e1ac099412
Kushal Patel <kushal300603@gmail.com>
2023-12-10 14:02:00 +0530

    updating userDeatils type

--- src/utils/types/userDetails.ts
+++ src/utils/types/userDetails.ts
@@ -1,10 +1,12 @@
 
-export type Userdetails = {
-    User_id: number;
+type Userdetails = {
+    User_id: string;
     username: string;
     email: string;
     DOB: Date;
     college: string;
     password: string;
+    classrooms: string[];
 };
 
+export default Userdetails;
