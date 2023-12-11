4486d5fb92c3f4be5713f1b80c035838691aa67a
malay44 <malay.p1@ahduni.edu.in>
2023-12-12 02:00:48 +0530

    get's realtime announcement handling in Class1 component

--- src/pages/classroom/[classId]/index.tsx
+++ src/pages/classroom/[classId]/index.tsx
@@ -1,5 +1,3 @@
-import NoCard from '@/components/Cards/NoCard';
-import List from '@/components/List/List';
 import ClassNavAbove from '@/components/Navbar/ClassNavAbove';
 import ClassNavBelow from '@/components/Navbar/ClassNavBelow';
 import Announcement from '@/components/Stream/Announcement';
@@ -8,39 +6,41 @@ import CreateAnnouncement from '@/components/Stream/CreateAnnouncement';
 import Upcoming from '@/components/Stream/Upcoming';
 import Topbar from '@/components/Topbar/Topbar';
 import { auth, firestore } from '@/firebase/firebase';
-import classroomDetails from '@/utils/types/classroom/classroomDetails';
-import contestDetails from '@/utils/types/contest/contestDetails';
-import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
-import { get } from 'http';
-import Link from 'next/link';
+import classroomDetails, { announcement } from '@/utils/types/classroom/classroomDetails';
+import { Timestamp, doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
+import { userTypeState } from '@/atoms/userTypeAtom';
+import { useRecoilState } from 'recoil';
 
 type class1Props = {
 
 };
 
 const Class1: React.FC<class1Props> = () => {
-    const [announcements, setAnnouncements] = useState<{ text: string; dateTime: string }[]>([]);
+    const [announcements, setAnnouncements] = useState<announcement[]>([]);
 
     const handleAddAnnouncement = (text: string) => {
-      const newAnnouncements = [...announcements, { text, dateTime: new Date().toLocaleString() }];
-      setAnnouncements(newAnnouncements);
+        const newAnnouncements = [{ content: text, createdAt: Timestamp.now() }, ...announcements] as announcement[];
+        const classroomRef = doc(firestore, 'classrooms', classId as string);
+        setDoc(classroomRef, { announcements: newAnnouncements }, { merge: true });
+        setAnnouncements(newAnnouncements);
     };
     const router = useRouter();
     const { classId } = router.query;
-
+    const [loading, setLoading] = useState(true);
     const [classroomDetails, setClassroomDetails] = useState<classroomDetails>({} as classroomDetails);
-    const [contests, setContests] = useState<contestDetails[]>([] as contestDetails[]);
+    const [usertype, setUserType] = useRecoilState(userTypeState);
     const [user] = useAuthState(auth);
 
     useEffect(() => {
+        let unsubscribe = () => { };
         const getClassroomData = async () => {
+            setLoading(true);
             const userRef = doc(firestore, "users", user!.uid);
             const userDoc = await getDoc(userRef);
-
             if (userDoc.exists()) {
                 const classroomsId = await userDoc.data().classrooms;
                 if (!classroomsId || classroomsId.length === 0) return;
@@ -50,21 +50,29 @@ const Class1: React.FC<class1Props> = () => {
                 const classroomDocs = await getDoc(classroomref);
                 if (!classroomDocs.exists()) return;
                 setClassroomDetails(classroomDocs.data() as classroomDetails);
+                classroomDocs.data().creatorID === user!.uid ? setUserType('teacher') : setUserType('student');
 
-                // getting contests data
-
-                if (!classroomDocs.data().contests || classroomDocs.data().contests.length === 0) {
-                    console.log('no contests');
-                    return;
-                };
-                const contestsQuery = query(
-                    collection(firestore, 'contest'),
-                    where('contestId', 'in', classroomDocs.data().contests)
-                );
-                const contestDocs = await getDocs(contestsQuery);
-                setContests(contestDocs.docs.map(doc => doc.data()) as contestDetails[]);
-                console.log(contestDocs.docs.map(doc => doc.data()));
-
+                // getting Announcements data
+                // if (!classroomDocs.data().announcements || classroomDocs.data().announcements.length === 0) {
+                //     console.log('no announcements');
+                //     return;
+                // };
+                // setAnnouncements(classroomDocs.data().announcements);
+                const classroomRef = doc(firestore, 'classrooms', classId as string);
+                unsubscribe = onSnapshot(classroomRef, (doc) => {
+                    if (!doc.exists()) {
+                        console.log('no announcements');
+                        return;
+                    }
+                    const classroomData = doc.data() as classroomDetails;
+                    if (!classroomData.announcements || classroomData.announcements.length === 0) {
+                        console.log('no announcements');
+                        return;
+                    }
+                    console.log('Announcements:', classroomData.announcements);
+                    setAnnouncements(classroomData.announcements);
+                });
+                setLoading(false);
             }
             else {
                 console.log('userDoc does not exist');
@@ -76,6 +84,11 @@ const Class1: React.FC<class1Props> = () => {
             getClassroomData();
         }
         if (!user) setClassroomDetails({} as classroomDetails);
+
+        return () => {
+            unsubscribe();
+            console.log('unsubscribed');
+        }
     }, [user]);
 
 
@@ -88,58 +101,26 @@ const Class1: React.FC<class1Props> = () => {
             {/* Stream  */}
 
             <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3 '>
-            <Banner/>
-            <div className='flex gap-7 w-full'>
-                <div className='relative flex flex-col gap-5 w-4/5 '>
-                    <CreateAnnouncement onAddAnnouncement={handleAddAnnouncement}/>
-                    {announcements.map((announcement, index) => (
-                      <Announcement key={index} text={announcement.text} dateTime={announcement.dateTime} />
-                    ))}
-                    {announcements.length === 0 && (<div className='flex items-center justify-center  p-5'>
+                <Banner />
+                <div className='flex gap-7 w-full'>
+                    <div className='relative flex flex-col gap-5 w-4/5 '>
+                        {usertype == 'teacher' && <CreateAnnouncement onAddAnnouncement={handleAddAnnouncement} />}
+                        {announcements.map((announcement, index) => (
+                            <Announcement key={index} text={announcement.content} dateTime={announcement.createdAt.toDate().toUTCString()} />
+                        ))}
+                        {announcements.length === 0 && (<div className='flex items-center justify-center  p-5'>
                             <div className='border-2 border-dashed p-4 rounded-lg'>
                                 <h3 className='text-dark-gray-6 font-medium text-lg'>
-                                    No Announcement
+                                    {loading ? "loading.." : "No Announcement"}
                                 </h3>
                             </div>
                         </div>)}
-                </div>
-                <div className='flex flex-col gap-3 w-1/5 '>
-                    <Upcoming/>
+                    </div>
+                    <div className='flex flex-col gap-3 w-1/5 '>
+                        <Upcoming />
+                    </div>
                 </div>
             </div>
-        </div>
-
-            {/* Classwork */}
-
-            {/* <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3 '>
-                <div className='border-b-2 border-b1 pb-2'>
-                    <h2 className='text-primary-blue text-xl font-semibold'>Contests</h2>
-                </div>
-                {contests && contests.map((contest) => (
-                    <Link key={contest.contestId} href={`${router.pathname}/${contest.contestId}`}>
-                      <List title={contest.description} dueDate={"due on " + contest.startTime.toDate().toUTCString()} />
-                  </Link>
-                ))}
-            </div> */}
-
-            {/* People */}
-
-            {/* <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3 '>
-
-        </div> */}
-
-            {/* Discussion */}
-
-            {/* <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3 '>
-
-        </div> */}
-
-            {/* Grades */}
-
-            {/* <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3 '>
-
-        </div> */}
-
         </main>
     </>
 }
