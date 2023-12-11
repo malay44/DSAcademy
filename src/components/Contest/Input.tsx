58c53d05e651a9994d1332b7c2955df9c18c5fd9
priyajani028 <priyajani028@gmail.com>
2023-12-11 11:58:33 +0530

    Update textarea secondary variant Input.tsx

--- src/components/Contest/Input.tsx
+++ src/components/Contest/Input.tsx
@@ -16,7 +16,7 @@ const Input: React.FC<InputProps> = ({ label, variant = 'primary', ...props }) =
         <textarea
           {...props}
           rows={variant === 'secondary' ? 4 : 1}
-          className={`bg-dark-gray-9 border-2 text-dark-gray-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 `}
+          className={`bg-dark-gray-9 border-2 ${variant === 'secondary' ? '' : 'resize-none'} text-dark-gray-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 `}
         />
       </WrapperComponent>
     </div>
