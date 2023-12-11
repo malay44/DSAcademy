68a8a51ea670db6112a0ef1359d59fa675a7a530
malay44 <malay.p1@ahduni.edu.in>
2023-12-11 12:36:22 +0530

    Fix issues with Newcontest component

--- src/pages/classroom/[classId]/classwork/newcontest.tsx
+++ src/pages/classroom/[classId]/classwork/newcontest.tsx
@@ -5,13 +5,40 @@ import NewProblem from '@/components/Contest/NewProblem';
 import DropdownMenu from '@/components/DropDownMenu/DropdownMenu';
 import ClassNavAbove from '@/components/Navbar/ClassNavAbove';
 import Topbar from '@/components/Topbar/Topbar';
-import React from 'react';
+import { firestore } from '@/firebase/firebase';
+import classrooms from '@/utils/types/classroom/classroomDetails';
+import { doc, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import React, { use, useEffect, useState } from 'react';
 
 type NewcontestProps = {
     
 };
 
 const Newcontest:React.FC<NewcontestProps> = () => {
+
+    const [classroomDetails, setClassroomDetails] = useState<classrooms>({} as classrooms);
+    
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router && router.query && router.query.classId) {
+            const classroomRef = doc(firestore, "classrooms", router.query.classId as string);
+            getDoc(classroomRef).then((doc) => {
+                if (doc.exists()) {
+                    console.log("Document data:", doc.data());
+                    setClassroomDetails(doc.data() as classrooms);
+                }
+                else {
+                    console.log("No such document!");
+                }
+            }
+            ).catch((error) => {
+                console.log("Error getting document:", error);
+            });
+        }
+    }, [router, router.query.classId]);
+
     const questionsData = [
         { name: 'Question 1', points: 10 },
         { name: 'Question 2', points: 15 },
@@ -21,7 +48,7 @@ const Newcontest:React.FC<NewcontestProps> = () => {
     return <>
     <main className='flex flex-col bg-white dark:bg-dark-layer-2 h-screen '>
         <Topbar />
-        <ClassNavAbove/>
+        <ClassNavAbove classroomId={classroomDetails.classroomId} classroomName={classroomDetails.classroomName}/>
         <div className='flex-1 w-full max-w-[1200px] mx-auto  py-5 flex flex-col gap-3  pr-3  overflow-auto'>
         <Input label="Contest Name" variant="primary" placeholder="Enter contest name" required />
         <div className='flex justify-between items-center border-b-2 b border-primary-blue pb-2'>
